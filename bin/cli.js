0bf730bf23c9ef5e26ce99db98834900f653718b
pranavmohantyy <mohantypranav147@gmail.com>
2026-07-28 10:29:55 -0700

    add find command for file search

--- bin/cli.js
+++ bin/cli.js
@@ -1,13 +1,29 @@
+const fs = require('fs');
+const path = require('path');
 const args = process.argv.slice(2);
 
 const commands = {
   greet: () => console.log('Hello!'),
-  goodbye: () => console.log('Goodbye!')
+  goodbye: () => console.log('Goodbye!'),
+  find: (pattern, dir) => {
+    const searchFiles = (dir) => {
+      fs.readdirSync(dir).forEach(file => {
+        const filePath = path.join(dir, file);
+        if (fs.statSync(filePath).isDirectory()) {
+          searchFiles(filePath);
+        } else if (file.includes(pattern)) {
+          console.log(filePath);
+        }
+      });
+    };
+    searchFiles(dir);
+  }
 };
 
 const command = args[0];
-
-if (commands[command]) {
+if (command === 'find' && args[1] && args[2]) {
+  commands.find(args[1], args[2]);
+} else if (commands[command]) {
   commands[command]();
 } else {
   console.log('Unknown command');
